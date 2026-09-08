8b4f80c8c8581b5eab8b0a9855f5c8bd74f2547a
Erwinsss <161535702+Erwinsss@users.noreply.github.com>
2026-09-08 12:38:25 -0600

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -11,3 +11,18 @@ function factorial(n) {
 console.log(factorial(5)); // 120
 console.log(factorial(0)); // 1
 console.log(factorial(7)); // 5040
+
+// agregando lo repetido 
+function factorial(n) {
+    // Caso base
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    // Caso recursivo
+    return n * factorial(n - 1);
+}
+
+// Ejemplo de uso
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
+console.log(factorial(7)); // 5040
